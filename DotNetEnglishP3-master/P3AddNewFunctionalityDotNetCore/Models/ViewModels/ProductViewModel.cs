853e81b9460f46e69acb31309d71cd498c9f406e
Floverflow <flo.cinbolat@gmail.com>
2025-07-17 16:49:17 +0200

    refactor: replace manual validation with data annotations

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ResourcesProductService = P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
     public class ProductViewModel
     {
-        [BindNever]
-        public int Id { get; set; }
+        [BindNever] public int Id { get; set; }
 
+        [Required(
+            ErrorMessageResourceName = "MissingName",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(
+            ErrorMessageResourceName = "MissingStock",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
+        [Range(
+            1,
+            int.MaxValue,
+            ErrorMessageResourceName = "StockNotGreaterThanZero",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
+        [RegularExpression(
+            @"^-?\d+$",
+            ErrorMessageResourceName = "StockNotAnInteger",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
         public string Stock { get; set; }
 
+        [Required(
+            ErrorMessageResourceName = "MissingPrice", 
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
+        [Range(
+            0.01,
+            double.MaxValue,
+            ErrorMessageResourceName = "PriceNotGreaterThanZero",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
+        [RegularExpression(
+            @"^-?\d+(\.\d{1,2})?$",
+            ErrorMessageResourceName = "PriceNotANumber",
+            ErrorMessageResourceType = typeof(ResourcesProductService))]
         public string Price { get; set; }
     }
 }
